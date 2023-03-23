14caeae7a41a1ced401570faf67fecd46538541e
Brandon Stubbs <brandon@bos-solutions.co.uk>
2023-03-23 13:14:16 -0400

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://redefine.io',
-  integrations: [sitemap()]
+  integrations: [sitemap(), tailwind()]
 });
