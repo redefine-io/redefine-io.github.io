c13df71f40098a57dd5d53823cbe6e0140a6ec32
Brandon Stubbs <brandon@bos-solutions.co.uk>
2023-03-23 13:32:50 -0400

    add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://redefine.io',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), mdx()]
 });
