ee0336058100e87eed62e34c5e39328fa8a37593
Brandon Stubbs <brandon@bos-solutions.co.uk>
2023-03-23 14:01:13 -0400

    add redefine brand colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"redefine-primary": "#FA8072",
+				"redefine-secondary": "#0B445D",
+			}
+		},
 	},
 	plugins: [],
 }
