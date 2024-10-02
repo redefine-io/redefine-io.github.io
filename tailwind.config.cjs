f20a6bca58a1d14212ada5a26612e9851c542284
Brandon Stubbs <brandon@bos-solutions.co.uk>
2024-10-02 13:42:26 -0400

    yeet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Montserrat Alternates", ...defaultTheme.fontFamily.sans]
-			},
-			colors: {
-				"redefine-primary": "#fa8072",
-				"redefine-secondary": "#1e435b",
-			}
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: ["Montserrat Alternates", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "redefine-primary": "#fa8072",
+        "redefine-secondary": "#1e435b",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
